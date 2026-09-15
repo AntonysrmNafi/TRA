59212753dcf57333e2833c8dfbe6d19e91d818d8
Antonysrm Nafi <205429236+AntonysrmNafi@users.noreply.github.com>
2026-09-15 11:00:56 +0600

    Remove hideStatusBar call from HistoryDetailActivity
    Removed call to hideStatusBar in onCreate method.

--- app/src/main/java/com/blockveil/tracker/remover/ui/HistoryDetailActivity.kt
+++ app/src/main/java/com/blockveil/tracker/remover/ui/HistoryDetailActivity.kt
@@ -8,7 +8,6 @@ import com.blockveil.tracker.remover.R
 import com.blockveil.tracker.remover.data.CleanedLinkEntity
 import com.blockveil.tracker.remover.databinding.ActivityHistoryDetailBinding
 import com.blockveil.tracker.remover.safety.Verdict
-import com.blockveil.tracker.remover.util.hideStatusBar
 import java.text.DateFormat
 import java.util.Date
 
@@ -19,7 +18,6 @@ class HistoryDetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        hideStatusBar()
         binding = ActivityHistoryDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.toolbar.setNavigationOnClickListener { finish() }
